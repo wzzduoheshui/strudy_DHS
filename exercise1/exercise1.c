bfac21e4306bd6ce76d102499be934e5a6385284
drink-more-hot-water-a <255154642@qq.com>
2022-02-06 23:27:48 +0800

    比特随课练习

---
+++ exercise1/exercise1.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	//计算c语言中内置类型的大小（所占内存的长度）
+//	printf("char=%d\n", sizeof(char));
+//	printf("short=%d\n", sizeof(short));
+//	printf("int=%d\n", sizeof(int));
+//	printf("long=%d\n", sizeof(long));
+//	printf("long long=%d\n", sizeof(long long));
+//	printf("double=%d\n", sizeof(double));
+//	printf("float=%d\n", sizeof(float));
+//	return 0;
+//}
+
+//int main()
+//{
+//	//输入两个数据求和
+//	int a, b, sum;
+//	a = 0;
+//	b = 0;
+//	printf("请输入需要求和的两个数据\n");
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("a+b=%d", sum);
+//	return 0;
+//}
