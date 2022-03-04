bb3077c9ec49e25b14b5c0a6ebee32a5007e8319
drink-more-hot-water-a <255154642@qq.com>
2022-03-04 23:51:18 +0800

    函数练习

--- function/function.c
+++ function/function.c
@@ -37,22 +37,146 @@
 //出现bug
 //当实参传给形参的时候，形参是实参的一份临时拷贝，对形参的修改不会影响实参
 //形参
-int swap(int x, int y)
-{
-	int c = 0;
-	c = x;
-	x = y;
-	y = c;
-}
+//int swap(int* pa, int* pb)
+//{
+//	int c = *pa;
+//	*pa = *pb;
+//	*pb = c;
+//	
+//}
+//
+//int main()
+//{
+//	int a;
+//	int b;
+//	//实参
+//	scanf("%d %d", &a, &b);
+//	printf("前a=%d b=%d\n", a, b);
+//	swap(&a, &b);
+//	//swap(a, b);
+//	printf("后a=%d b=%d\n", a, b);
+//	return 0;
+//}
+
+
+
+
+
+
+//写一个函数可以判断一个数是不是素数
+
+//int prime(int a)
+//{
+//	int b=a;
+//	int i;
+//	for (i = 2; i < b; i++)
+//	{
+//		if ((b % i) == 0)//判断是否可以整除
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	//所输入的数a除上从2到a
+//	//如能除尽则就不为素数
+//	//利用取余原则，只要有一次可以整除则确认不是素数
+//	//如果函数返回1则为素数，返回0则不是素数
+//	//注：1既不是素数也不是合数
+//	if (a = 1)
+//	{
+//		printf("%d既不是素数也不是合数", a);
+//	}
+//	if (prime(a) == 1)
+//	{
+//		printf("%d是素数\n", a);
+//	}
+//	else
+//	{
+//		printf("%d不是素数\n", a);
+//
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+//写一个函数判断一年是不是闰年
+
+//int is_leap_year(int year)
+//{
+//	if ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0)
+//	{
+//		return 0;
+//	}
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	int year;
+//	//如果可以被400整除，则是闰年
+//	//如果可以被4整除但不能被100整除，则是闰年
+//	//若为闰年则返回0，否则返回1
+//	scanf("%d", &year);
+//	if (is_leap_year(year) == 0)
+//	{
+//		printf("%d是闰年", year);
+//	}
+//	else
+//		printf("%d不是闰年", year);
+//	return 0;
+//}
+
+//简化
+//*C语言 判断时为真返回1，为假返回0
+
+//int is_leap_year(int year)
+//{
+//	if ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0);
+//	//判断时为真返回1，为假返回0
+//}
+//int main()
+//{
+//	int year;
+//	//如果可以被400整除，则是闰年
+//	//如果可以被4整除但不能被100整除，则是闰年
+//	//若为闰年则返回0，否则返回1
+//	scanf("%d", &year);
+//	if (is_leap_year(year) == 0)
+//	{
+//		printf("%d是闰年", year);
+//	}
+//	else
+//		printf("%d不是闰年", year);
+//	return 0;
+//}
+
+
+
+
+//写一个函数，实现一个整形有序数组的二分查找。
 
 int main()
 {
-	int a;
-	int b;
-	//实参
-	scanf("%d %d", &a, &b);
-	printf("前a=%d b=%d\n", a, b);
-	swap(a, b);
-	printf("后a=%d b=%d\n", a, b);
+	//去哪里查--arr
+	//查谁----k
+	//怎么查----二分法
+	//一共有几个元素--用sizeof
+	//
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//arr中的元素个数=arr总共所占的字节/一个元素所占的字节
+	scanf("%d", &k);
+	find_arr(arr, k, sz);
+	printf("找到了，下标是%d", a);
 	return 0;
 }
