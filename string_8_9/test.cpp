dd784aa29649f5f53ec60590cc61fee8d51ee84d
drink-more-hot-water-a <255154642@qq.com>
2022-12-30 23:30:35 +0800

    string_8_9

--- string_8_9/test.cpp
+++ string_8_9/test.cpp
@@ -1,27 +1,53 @@
 #define  _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	//string str("Hello bit.");
+//	//cout << str << endl;
+//	//cout << str.size() << ":" << str.capacity() << endl;
+//	//cout << endl;
+//	//str.reserve(111);
+//	//cout << str << endl;
+//	//cout << str.size() << ":" << str.capacity() << endl;
+//	//cout << endl;
+//	//str.resize(5);
+//	//cout << str << endl;
+//	//cout << str.size() << ":" << str.capacity() << endl;
+//	//cout << endl;
+//	//str.resize(10);
+//	//cout << str << endl;
+//	//cout << str.size() << ":" << str.capacity() << endl;
+//	//cout << endl;
+//	//str.reserve(50);
+//	//cout << str << endl;
+//	//cout << str.size() << ":" << str.capacity() << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s1("abcdefg");
+//	string s2 = "abcdefg";
+//
+//	return 0;
+//}
+
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
-	string str("Hello bit.");
-	cout << str << endl;
-	cout << str.size() << ":" << str.capacity() << endl;
-	cout << endl;
-	str.reserve(111);
-	cout << str << endl;
-	cout << str.size() << ":" << str.capacity() << endl;
-	cout << endl;
-	str.resize(5);
-	cout << str << endl;
-	cout << str.size() << ":" << str.capacity() << endl;
-	cout << endl;
-	str.resize(10);
-	cout << str << endl;
-	cout << str.size() << ":" << str.capacity() << endl;
-	cout << endl;
-	str.reserve(50);
-	cout << str << endl;
-	cout << str.size() << ":" << str.capacity() << endl;
+	string s1("abcdefg");
+	string s2 = "abcdefg";
+
+	cout << s1 << endl;
+	cout << s2 << endl;
+	cin >> s2;
+	cout << endl << s2 << endl;
 	return 0;
 }
