f130363c8a4e7212c6987f3c8dfeafc8c80b830e
drink-more-hot-water-a <255154642@qq.com>
2023-07-30 10:00:15 +0800

    static_and_extern

---
+++ 3_C++_class/3_class.cpp
@@ -0,0 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include "3_class.h"
+
+
+
+int main()
+{
+	text();
+	cout << "main():" << a << "," << &a << endl;
+	return 0;
+}
---
+++ 3_C++_class/test.cpp
@@ -0,0 +1,10 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include "3_class.h"
+
+int text()
+{
+	cout << "text():" << a << "," << &a << endl;
+	a++;
+	return 0;
+}
