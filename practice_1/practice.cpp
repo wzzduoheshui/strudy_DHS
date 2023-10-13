d9e2a74cab1570cacce2cadf91ed58b0af78c439
drink-more-hot-water-a <255154642@qq.com>
2023-10-13 22:13:50 +0800

    牛客练习

---
+++ practice_1/practice.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int get_range(const vector<int> arr)
+{
+    int max = 0;
+    int min = 0;
+    vector<int>::iterator it = arr.begin();
+    while (it != arr.end())
+    {
+
+    }
+    return
+}
+
+int get_weights(const vector<int> arr)
+{
+
+}
+
+int main() {
+
+}
+
+//int main() {
+//    int n = 0;//排列的长度
+//    cin >> n;
+//    vector<int> arr;
+//    for (int i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        cin >> j;
+//        arr.push_back(j);
+//    }
+//    int x = 0, y = 0;
+//    cin >> x >> y;
+//    vector<int>::iterator it = arr.begin();
+//    while (it != arr.end())
+//    {
+//        if (((*it) == x && *(it + 1) == y) || ((*it) == y && *(it + 1) == x))
+//        {
+//            cout << "YES" << endl;
+//            return 0;
+//        }
+//        it++;
+//    }
+//    cout << "NO" << endl;
+//    return 0;
+//}
