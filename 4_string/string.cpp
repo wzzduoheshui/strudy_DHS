10a27e649cb52a331e753e4cff9e37515bd6dd62
drink-more-hot-water-a <255154642@qq.com>
2023-10-13 22:12:34 +0800

    模拟实现string

--- 4_string/string.cpp
+++ 4_string/string.cpp
@@ -4,16 +4,22 @@ using namespace MyString;
 int main()
 {
 	mstr str("hello world!");
-	cout << str << endl;
+	//cout << str << endl;
+	//cout << str.getsize() << endl;
+	//cout << str.getcapacity() << endl;
+
+	mstr s2(str);
+	cout << "s2:" << s2 << "size:" << s2.getsize() << endl;
+
 	//mstr s1(str);
 	//cout << s1 << endl;
 	//mstr s2;
 	//cout << s2 << endl;
 	//mstr s3(s2);
 	//cout << s3 << endl;
-	mstr s1("apple");
-	cout << s1 << endl;
-	s1 += str;
+	//mstr s1("apple");
+	//cout << s1 << endl;
+	//s1 += str;
 	//cout << s1 << endl;
 	return 0;
 }
--- 4_string/string_class.hpp
+++ 4_string/string_class.hpp
@@ -11,31 +11,54 @@ namespace MyString
 	class mstr
 	{
 	public:
-		friend ostream& operator<<(ostream& cout, const mstr& str);
+		friend ostream& operator<<(ostream& cout, const mstr& str);//cout
+		friend ostream& operator>>(ostream& cin, const mstr& str);//cin
 
-		mstr(const char* str = "")
+		mstr(const char* str = "")//构造
 		{
 			_size = strlen(str);
 			_capacity = _size;
+			cout << "mstr(const char* str = "")" << str << "size:" << _size << endl;
 			_str = new char[_capacity + 1];
-			memcpy(_str, str, _capacity);
+			memcpy(_str, str, _size);
+			_str[_size + 1] = '\0';
 		}
 
-		mstr(const mstr& str)
+		//mstr(const mstr& str)//拷贝构造
+		//{
+		//	this->_capacity = str._capacity;
+		//	this->_size = str._size;
+		//	this->_str = new char[_capacity+1];
+		//	memcpy(_str, str._str, _capacity);
+		//}
+
+		// 拷贝构造
+		void mswap(mstr& str)
 		{
-			this->_capacity = str._capacity;
-			this->_size = str._size;
-			this->_str = new char[_capacity+1];
-			memcpy(_str, str._str, _capacity);
+			//cout << str._size << "   " << str._capacity << "    " << str._str << endl;
+			std::swap(this->_str, str._str);
+			std::swap(this->_capacity, str._capacity);
+			std::swap(this->_size, str._size);
+		}
+		mstr(const mstr& str)
+			:_str(nullptr)
+			,_size(0)
+			,_capacity(0)
+		{
+			cout << "mstr(const mstr& str):" << str._str << endl << "size" << str._size << "capacity" << str._capacity << endl;
+			mstr tmp(str._str);
+			//cout << tmp << "size" << tmp._size << "capacity" << tmp._capacity << endl;
+			this->mswap(tmp);
 		}
 
-		~mstr()
+
+		~mstr()//析构
 		{
 			delete[] _str;
 			_size = _capacity = 0;
 		}
 
-		mstr operator+=(const mstr& str)
+		mstr operator+=(const mstr& str)//+=
 		{
 			mstr new_str;
 			new_str._capacity = str._capacity + this->_capacity + 1;
@@ -51,14 +74,19 @@ namespace MyString
 			return new_str;
 		}
 
-		size_t getsize()
+		size_t getsize()//获取size
 		{
 			return this->_size;
 		}
 
-		size_t getcapacity()
+		size_t getcapacity()//获取capacity
 		{
 			return this->_capacity;
+		}
+
+		void swap(mstr str)
+		{
+			
 		}
 
 	private:
@@ -75,4 +103,12 @@ namespace MyString
 		}
 		return cout;
 	}
+
+	inline ostream& operator>>(ostream& cin, const mstr& str)
+	{
+		mstr buffe;
+		
+
+	}
+
 }
