e00d2ee3c832bdd63695ef0581ae2ba04ff83c55
drink-more-hot-water-a <255154642@qq.com>
2023-11-24 01:03:21 +0800

    有效三角形

--- Blue/test2/1089.cpp
+++ Blue/test2/1089.cpp
@@ -1,49 +1,143 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
+#include <algorithm> 
 using namespace std;
 
-//快乐数（https://leetcode.cn/problems/happy-number/）
+//class Solution {
+//public:
+//    int triangleNumber(vector<int>& nums) {
+//        size_t size = nums.size();
+//        std::sort(nums.begin(), nums.end());
+//        int sum = 0;
+//        for (int c = size - 1; c >= 2; --c)
+//        {
+//            int a = 0;
+//            int b = c - 1;
+//            while (a < b && b < c)
+//            {
+//                if (nums[a] + nums[b] > nums[c])
+//                {
+//                    sum += (b - a);
+//                    --b;
+//                    a = 0;
+//                }
+//                else
+//                {
+//                    ++a;
+//                }
+//            }
+//        }
+//        return sum;
+//    }
+//    
+//    void print(const vector<int>& nums)
+//    {
+//        for (auto e : nums)
+//        {
+//            cout << e << " ";
+//        }
+//        cout << endl;
+//    }
+//};
 
-int numHappy(int n)
-{
-    int sum = 0;
-    while (n > 0)
-    {
-        int a = n % 10;
-        sum += a * a;
-        n /= 10;
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        size_t size = nums.size();
+        int sum = 0;
+        for (int c = size - 1; c > 1; --c)
+        {
+            for (int b = c - 1; b > 0; --b)
+            {
+                int left = 0;
+                int right = b - 1;
+                int mid = (left + right) / 2;
+                int k = b;
+                while (left < right)
+                {
+                    mid = (left + right) / 2;
+                    if (nums[mid] + nums[b] <= nums[c])
+                    {
+                        k = mid;
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid;
+                    }
+                }
+                sum += b - mid;
+            }
+        }
+        return sum;
     }
-    return sum;
-}
 
-bool isHappy(int n) 
-{
-    int slow = numHappy(n);
-    int fast = numHappy(numHappy(n));
-
-    while (fast != slow)
+    void print(const vector<int>& nums)
     {
-        fast = numHappy(numHappy(fast));
-        slow = numHappy(slow);
+        for (auto e : nums)
+        {
+            cout << e << " ";
+        }
+        cout << endl;
     }
 
-    if (fast == 1)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+};
 
 int main()
 {
-    int num = 19;
-    cout << isHappy(num) << endl;
+	vector<int> nums = { 2,3,2,4 };
+    Solution s;
+    s.print(nums);
+    cout << s.triangleNumber(nums) << endl;
+    s.print(nums);
 	return 0;
 }
+
+
+
+//快乐数（https://leetcode.cn/problems/happy-number/）
+
+//
+//int numHappy(int n)
+//{
+//    int sum = 0;
+//    while (n > 0)
+//    {
+//        int a = n % 10;
+//        sum += a * a;
+//        n /= 10;
+//    }
+//    return sum;
+//}
+//
+//bool isHappy(int n) 
+//{
+//    int slow = numHappy(n);
+//    int fast = numHappy(numHappy(n));
+//
+//    while (fast != slow)
+//    {
+//        fast = numHappy(numHappy(fast));
+//        slow = numHappy(slow);
+//    }
+//
+//    if (fast == 1)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+//
+//int main()
+//{
+//    int num = 19;
+//    cout << isHappy(num) << endl;
+//	return 0;
+//}
 
 //void test(vector<int>& arr)
 //{
